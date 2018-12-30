479d65f5aed15d82cbcd72dda397bdcccb78e0e6
Marie-Carrere <marie.carrere@icloud.com>
2018-12-30 10:55:12 +0100

    oauth login

---
+++ config/passport-setup.js
@@ -0,0 +1,12 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20');
+const keys = require('./keys');
+
+passport.use(
+    new GoogleStrategy({
+        //options for the Google Strategy
+        clientID: keys.google.clientID,
+        clientSecret: keys.google.clientSecret
+    }), () => {
+        //passport callback function
+    })
