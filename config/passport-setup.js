066e5f7f354e96ff23716c8ef33a3920dd16ec7f
Marie-Carrere <marie.carrere@icloud.com>
2019-01-10 13:47:36 +0100

    retrieving Users

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('./keys');
+const User = require('../models/user-model');
 
 passport.use(
     new GoogleStrategy({
@@ -9,6 +10,18 @@ passport.use(
         clientID: keys.google.clientID,
         clientSecret: keys.google.clientSecret
     }, (accessToken, refreshToken, profile, done) => {
-        //passport callback function
+        //check if user already exists in database
+        User.findOne({googleid: profile.id}).then((currentUser) => {
+            if(currentUser) {
+                console.log('user is: ', currentUser);
+            } else {
+                new User({
+                    username: profile.displayName,
+                    googleid: profile.id
+                }).save().then((newUser) => {
+                    console.log('new user created:' + newUser);
+                });
+            }
+        });
     })
 )
